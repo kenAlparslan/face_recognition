ca39e602739d8e3091ee999ddb9d4cf07b901e63
Ken <ken@alparslan@gmail.com>
2020-02-12 12:57:32 -0500

    changed ImageLinkForm component

--- face_recognition/src/components/ImageLinkForm/ImageLinkForm.js
+++ face_recognition/src/components/ImageLinkForm/ImageLinkForm.js
@@ -6,9 +6,11 @@ const ImageLinkForm = () => {
 			<p className="f3">
 				{'This program will detect faces in your pictures. Give it a try'}
 			</p>
-			<div>
-				<input className="f4 pa2 w-70 center" type ="text" />
-				<button>Detect</button>
+			<div className='center'>
+				<div className='pa4 br3 shadow-5'>
+					<input className="f4 pa2 w-70 center" type ="text" />
+					<button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple">Detect</button>
+				</div>			
 			</div>
 		</div>
 	);
