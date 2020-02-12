8651e3d9dbf50ead9f0a726d8d163313defc06b3
Ken <47366236+kenAlparslan@users.noreply.github.com>
2020-02-12 15:11:30 -0500

    implemented particle.js and rank component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,46 @@ import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import Particles from 'react-particles-js';
+
+const particlesOptions = {
+	particles: {
+		number: {
+			value: 150,
+			density: {
+				enable: true,
+				value_area: 800
+			}
+		}
+	},
+	interactvity: {
+		detect_on: 'window',
+		events: {
+			onhover: {
+				enable: true,
+				mode: 'repulse'
+			},
+			resize: true
+		},
+		modes: {
+			repulse: {
+				distance: 200,
+				duration: 0.4
+			}
+		}
+	}
+}
 
 function App() {
   return (
     <div className="App">
+    <Particles className='particles'
+              params={particlesOptions}
+      />
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
       {/*
         <FaceRecongnition /> */}
---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Rank = () => {
+	return (
+		<div className='white f3'>
+			<div>
+				{'Ken, your current rank is...' }
+			</div>
+			<div className='white f1'>
+				{'#5' }
+			</div>
+		</div>
+	);
+}
+
+export default Rank;
