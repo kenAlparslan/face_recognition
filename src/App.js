5de12bd82389f5d08b8680ad65aec55729b22e32
Ken <47366236+kenAlparslan@users.noreply.github.com>
2020-02-19 14:01:47 -0500

    testing signin functionality with backend

--- src/App.js
+++ src/App.js
@@ -56,6 +56,12 @@ class App extends React.Component {
 		}
 	}
 
+	// componentDidMount() {
+	// 	fetch('http://localhost:3000/')
+	// 	.then(resp => resp.json())
+	// 	.then(console.log)
+	// }
+
 	calculateFaceLocation = (data) => {
 		const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
 		const image = document.getElementById('inputimage');
