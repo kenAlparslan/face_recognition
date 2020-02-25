d8a2fab4ef2402d3ac9ff4c61da3403817d82427
Ken <47366236+kenAlparslan@users.noreply.github.com>
2020-02-25 13:59:30 -0500

    deploy

--- src/App.js
+++ src/App.js
@@ -100,7 +100,7 @@ class App extends React.Component {
 
 	onPictureSubmit = () => {
 		this.setState({imageURL: this.state.input})
-		fetch('http://localhost:3000/imageurl', {
+		fetch(' https://tranquil-journey-73190.herokuapp.com/imageurl', {
 			method: 'post',
 			headers: {'Content-Type': 'application/json'},
 				body: JSON.stringify({
@@ -111,7 +111,7 @@ class App extends React.Component {
 		.then(response => {
 			if(response)
 			{
-				fetch('http://localhost:3000/image', {
+				fetch(' https://tranquil-journey-73190.herokuapp.com/image', {
 					method: 'put',
 					headers: {'Content-Type': 'application/json'},
 					body: JSON.stringify({
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -24,7 +24,7 @@ class Register extends React.Component {
 	}
 
 	onSubmitSignIn = () => {
-		fetch('http://localhost:3000/register', {
+		fetch('https://tranquil-journey-73190.herokuapp.com/register', {
 			method: 'post',
 			headers: {'Content-Type': 'application/json'},
 			body: JSON.stringify({
